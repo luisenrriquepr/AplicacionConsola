06f70ce4064e8bd8e2bb620426d92491f8649a01
luisenrriquepr <44734347+luisenrriquepr@users.noreply.github.com>
2024-08-02 14:53:51 -0500

    Cargando proyecto al Repositorio_02082024
    Cargando proyecto al Repositorio_02082024

---
+++ Calculadora_Basica/Calculadora_Basica/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce el primer número:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Introduce la operación (+, -, *, /):");
+        string operacion = Console.ReadLine();
+
+        double resultado = 0;
+
+        switch (operacion)
+        {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Operación no válida");
+                break;
+        }
+
+        Console.WriteLine("El resultado es: " + resultado);
+    }
+}
