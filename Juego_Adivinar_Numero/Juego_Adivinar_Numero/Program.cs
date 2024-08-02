51b3d961a6d8be5023fbeae2225340c6fb805ce8
luisenrriquepr <44734347+luisenrriquepr@users.noreply.github.com>
2024-08-02 14:54:52 -0500

    Cargando proyecto al Repositorio_02082024
    Cargando proyecto al Repositorio_02082024

---
+++ Juego_Adivinar_Numero/Juego_Adivinar_Numero/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class AdivinarNumero
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101);
+        int intentos = 0;
+        int adivinanza = 0;
+
+        Console.WriteLine("Adivina el número entre 1 y 100:");
+
+        while (adivinanza != numeroSecreto)
+        {
+            adivinanza = Convert.ToInt32(Console.ReadLine());
+            intentos++;
+
+            if (adivinanza < numeroSecreto)
+            {
+                Console.WriteLine("Demasiado bajo. Intenta de nuevo:");
+            }
+            else if (adivinanza > numeroSecreto)
+            {
+                Console.WriteLine("Demasiado alto. Intenta de nuevo:");
+            }
+        }
+
+        Console.WriteLine($"¡Felicidades! Adivinaste el número en {intentos} intentos.");
+    }
+}
